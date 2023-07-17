0d2ded41177e84f0239f5f523e9d99cc12a473fb
Butch Griffin <butchg@comcast.net>
2023-07-17 16:36:50 -0700

    removed senstive info

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 npm run build
-cp -r run.sh package.json package-lock.json dist content security ~/deploy/partdb
-pushd ~/deploy/partdb
+if [ ! -f "~/deploy/xerodb" ]; then
+    echo Making directory xerodb
+    mkdir -p ~/deploy/xerodb
+fi
+
+cp -r run.sh package.json package-lock.json dist content security ~/deploy/xerodb
+pushd ~/deploy/xerodb
 npm install
 popd
